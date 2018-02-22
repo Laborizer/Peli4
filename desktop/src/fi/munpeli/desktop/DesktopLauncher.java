0c0a5dfbb96c59ac2b88347e15ff174c1cfcd4e5
Lauri Laiho <laiho.lauri@luukku.com>
2018-02-22 15:44:28 +0200

    Update

---
+++ desktop/src/fi/munpeli/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package fi.munpeli.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.munpeli.MarioSideScroller;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new MarioSideScroller(), config);
+	}
+}
